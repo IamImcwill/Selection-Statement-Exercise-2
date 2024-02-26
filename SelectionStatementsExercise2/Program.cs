70bedcc338020af359c88a0c0e127965aaef3193
Caris Williams <cwill1013@gmail.com>
2024-02-26 12:32:10 -0500

    Inital Commit

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite school subject?");
+            
+            var subject = Console.ReadLine();
+
+            switch (subject) 
+            {
+                case "Math":
+                    Console.WriteLine("Math is awesome!");
+                    break;
+                case "Science":
+                    Console.WriteLine("Science is my 2nd favorite!");
+                    break;
+                case "English":
+                    break; 
+                    Console.WriteLine("Was my least favorite!");
+                    break;
+                case "History":
+                    Console.WriteLine("I did really care for History either!");
+                    break;
+                default:
+                    Console.WriteLine($"Ah I haven't taken that subject before. {subject} sounds fun!");
+                    break;
+            }
         }
     }
 }
